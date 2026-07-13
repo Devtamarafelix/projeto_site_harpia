e6812cdc0c51e8cf86550033d7e723ec423d9722
Dev Tâmara <tamarafelix63@gmail.com>
2026-07-13 20:38:40 -0300

    feat: integração do backEnd com o frontEnd

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,2 +1,37 @@
 // plugin VLibras
 new window.VLibras.Widget('https://vlibras.gov.br/app')
+
+//intregração com o backend
+const btnEntrar = document.getElementById('btn-entrar');
+btnEntrar.addEventListener('click', async () => {
+    const identificador = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    if(!identificador || !senha) {
+        alert('Preencha todos os campos');
+        return;
+    }
+
+    try {
+        const resposta = await fetch ('https://projeto-site-harpia.onrender.com/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ identificador, senha })
+        })
+
+        const dados = await resposta.json()
+        if (!resposta.ok) {
+            alert(dados.error);
+            return
+        }
+
+        localStorage.setItem('token', dados.token);
+        localStorage.setItem('usuarioLogado', JSON.stringify(dados.usuario));
+
+        window.location.href = 'pg-usuario.html';
+
+    } catch (error) {
+        console.error('Erro ao conectar com o servidor:', error);
+        alert('Não foi possível conectar ao servidor. Verifique se o backend está funcionando.');
+    }
+})
